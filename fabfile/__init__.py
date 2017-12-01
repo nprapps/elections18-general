f372b00847b324e0ad222ea117cebeb74f9b9475
jjelosua <jelosua@npr.org>
2017-12-01 10:42:40 -0500

    rename deployment functions

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -117,16 +117,16 @@ code to a remote server if required.
 """
 
 @task
-def deploy_data():
+def deploy_results():
     local('rm -rf {0}'.format(app_config.DATA_OUTPUT_FOLDER))
     local('mkdir {0}'.format(app_config.DATA_OUTPUT_FOLDER))
 
     render.render()
-    deploy_results()
+    move_s3()
 
 
 @task
-def deploy_results():
+def move_s3():
     local('aws s3 cp {0}/alabama-results.json s3://{1}/{2}/data/ --acl public-read --cache-control max-age=5'.format(app_config.DATA_OUTPUT_FOLDER, app_config.S3_BUCKET, app_config.PROJECT_SLUG))
 
 
