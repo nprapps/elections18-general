2182221fd33f16b0c3ba40449aa8d88c3b83c211
jjelosua <jelosua@npr.org>
2017-12-04 12:44:21 -0500

    use npr_winner as our unique winner source of truth

--- fabfile/render.py
+++ fabfile/render.py
@@ -33,8 +33,7 @@ COMMON_SELECTIONS = [
     models.Result.statename,
     models.Result.statepostal,
     models.Result.votepct,
-    models.Result.votecount,
-    models.Result.winner
+    models.Result.votecount
 ]
 
 SENATE_SELECTIONS = COMMON_SELECTIONS + [
@@ -63,7 +62,7 @@ def _serialize_results(results, selections, key='statepostal'):
 
         for result in results:
             result_dict = model_to_dict(result, backrefs=True, only=selections)
-            result_dict['npr_winner'] = result.is_npr_winner()
+            result_dict['winner'] = result.is_npr_winner()
 
             dict_key = result_dict[key]
             if not serialized_results['results'].get(dict_key):
